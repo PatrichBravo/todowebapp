9765c1debc5d82ee5bcb650a7150df2df8b6d044
PatrichBravo <patrich.bravo@gmail.com>
2024-03-15 21:51:10 +1300

    wep py app

--- web.py
+++ web.py
@@ -17,8 +17,13 @@ st.write("This app is to increase your productivity")
 st.checkbox("Buy Grocery")
 st.checkbox("Throw the trash")
 
-for todo in todos:
-    st.checkbox(todo)
-    
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.experimental_rerun()
+        
 st.text_input(label="", placeholder="Add new todo...",
               on_change=add_todo, key='new_todo')
