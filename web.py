4aa83413dd55a849c822abfbdd9563ebe8a06c1d
PatrichBravo <patrich.bravo@gmail.com>
2024-03-15 22:15:22 +1300

    changes in layout and pages

--- web.py
+++ web.py
@@ -3,6 +3,8 @@ import functions
 
 todos = functions.get_todos()
 
+#st.set_page_config(layout="wide") #to chanege the layout
+
 def add_todo():
     todo = st.session_state["new_todo"] + "\n"
     todos.append(todo)
@@ -12,10 +14,13 @@ def add_todo():
 
 st.title("My Todo app")
 st.subheader("This is my todo app")
-st.write("This app is to increase your productivity")
+st.write("This app is to increase your <b>productivity</b>", unsafe_allow_html=True) #you could yous html in write metod.
 
-st.checkbox("Buy Grocery")
-st.checkbox("Throw the trash")
+#st.checkbox("Buy Grocery")
+#st.checkbox("Throw the trash")
+
+st.text_input(label="", placeholder="Add new todo...",
+              on_change=add_todo, key='new_todo')
 
 for index, todo in enumerate(todos):
     checkbox = st.checkbox(todo, key=todo)
@@ -25,5 +30,4 @@ for index, todo in enumerate(todos):
         del st.session_state[todo]
         st.experimental_rerun()
         
-st.text_input(label="", placeholder="Add new todo...",
-              on_change=add_todo, key='new_todo')
+
